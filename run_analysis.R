89b7c8276b3eef0f7018ab384ed6a88bab99b0b5
Peter Suhner <p99@suhner.info>
2014-08-14 10:50:59 +0200

    minor amendments to run_analysis.R
    - reformatting column names to camelCase
    - redefinition of certain columns

--- run_analysis.R
+++ run_analysis.R
@@ -31,67 +31,71 @@ setwd(paste(WD_BASE, WD_INPUT, sep = "/"))
 # ----------
 # 1. Create one test data set from the training and test data
 ## read features data
-  tr <- read.table("train/X_train.txt")
-  te <- read.table("test/X_test.txt")
-  features <- rbind(tr, te)
+tr <- read.table("train/X_train.txt")
+te <- read.table("test/X_test.txt")
+features <- rbind(tr, te)
 
 ## read activities data
-  tr <- read.table("train/y_train.txt")
-  te <- read.table("test/y_test.txt")
-  activities <- rbind(tr, te)
+tr <- read.table("train/y_train.txt")
+te <- read.table("test/y_test.txt")
+activities <- rbind(tr, te)
 
 ## read subjects
-  tr <- read.table("train/subject_train.txt")
-  te <- read.table("test/subject_test.txt")
-  subject <- rbind(tr, te)
+tr <- read.table("train/subject_train.txt")
+te <- read.table("test/subject_test.txt")
+subject <- rbind(tr, te)
 
 ## read activity labels
-  act_lbl <- read.table("activity_labels.txt")
-  names(act_lbl) <- c("num", "name")
+act_lbl <- read.table("activity_labels.txt")
+names(act_lbl) <- c("num", "name")
 
 ## read feature names
-  feature_names <- read.table("features.txt")
-  names(feature_names) <- c("num", "name")
+feature_names <- read.table("features.txt")
+names(feature_names) <- c("num", "name")
 
 # 2. Extract the mean and std deviation data for each measurement
-## throw away obsolete features (anything apart from 'mean' and 'std')
-  feature_names <- feature_names[grep("mean|std", feature_names$name), ]
+## throw away obsolete features (anything apart from 'mean(' and 'std(')
+feature_names <- feature_names[grep("mean\\(|std\\(", feature_names$name), ]
 
 ## reduce content of in_x to the required features
-  features <- features[, feature_names$num]
+features <- features[, feature_names$num]
 
 # 3. Apply descriptive names to the activities
-  activities$V1 <- act_lbl[activities$V1,]$name
+activities$V1 <- act_lbl[activities$V1,]$name
 
 # 4. Apply descriptive names to the variable names
-## eliminate the brackets from the column the names
-  feature_names$name <- gsub("\\(\\)","", feature_names$name)
+## eliminate the brackets from the column names
+## and reformat names to camelCase
+feature_names$name <- gsub("\\(\\)","", feature_names$name)
+feature_names$name <- gsub("std","Std", feature_names$name)
+feature_names$name <- gsub("mean","Mean", feature_names$name)
+feature_names$name <- gsub("-","", feature_names$name)
 
-  names(features)   <- feature_names$name
-  names(activities) <- c("Activity")
-  names(subject)    <- c("Subject")
+names(features)   <- feature_names$name
+names(activities) <- c("Activity")
+names(subject)    <- c("Subject")
 
 ## create a single data fram with tidy data
-  tidyData <- cbind(subject, activities, features)
+tidyData <- cbind(subject, activities, features)
 
 ## write the tidy data to a file in CSV format
-  write.csv(tidyData, paste(WD_BASE,"tidyData.csv",sep = "/"))
+write.csv(tidyData, paste(WD_BASE,"tidyData.csv",sep = "/"))
 
 # 5. Create second, independent tidy data set with the average of each variable for each activity and each subject
-  aggregation_list <- list(tidyData$Subject, tidyData$Activity)
-  
+aggregation_list <- list(tidyData$Subject, tidyData$Activity)
+
 ## get the number of columns for tidy data dataset
-  dim_tidyData <- dim(tidyData)[2]
-  
+dim_tidyData <- dim(tidyData)[2]
+
 ## aggregate the data to build averages
-  averages <- aggregate(tidyData[, 3:dim_tidyData], aggregation_list, FUN = mean)
-  
+averages <- aggregate(tidyData[, 3:dim_tidyData], aggregation_list, FUN = mean)
+
 ## provide proper names for the aggregation columns
-  feat_names <- c("Subject", "Activity", as.character(feature_names$name))
-  names(averages)  <- feat_names
-  
+feat_names <- c("Subject", "Activity", as.character(feature_names$name))
+names(averages)  <- feat_names
+
 ## swap the first two columns (as data is ordered by Activity and Subject)
-  averages <- averages[, c(2,1,3:dim_tidyData)]
+averages <- averages[, c(2,1,3:dim_tidyData)]
 
 ## write the tidy data to a file in requested format
-  write.table(averages, file = paste(WD_BASE, "tidyData_averages.txt",sep = "/"), row.name = FALSE)
+write.table(averages, file = paste(WD_BASE, "tidyData_averages.txt",sep = "/"), row.name = FALSE)
